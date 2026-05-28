fe186e929813a6d800dc567189e0b23e824c8a51
DaniDanissa <dan.mirandac@duocuc.cl>
2026-05-27 21:02:31 -0400

    Add files via upload

--- microtonos.py
+++ microtonos.py
@@ -58,6 +58,11 @@ while ejecutando:
                 winsound.beep(440,150)
         except ValueError:
             print("Error, debes colocar un numero entero")
+    elif opcion == 4:
+        print(f"Hay {microtonos_activos} microtonos activos haciendo vibrar el ambiente ")
+    elif opcion == 5:
+        print("Saliendo del sistema")
+        ejecutando = False
     else:
         print("Error")
 
